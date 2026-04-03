4dec7106fea017735e7a888d754923ae1165c125
wagamama22 <wagajob@gmail.com>
2026-04-04 00:26:52 +0100

    safeAreaView and text formatting

--- App.tsx
+++ App.tsx
@@ -1,29 +1,49 @@
-
-import { StyleSheet, Text, View } from 'react-native';
-import{SafeAreaProvider, SafeAreaView}from "react-native-safe-area-context";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
+      {/* This is for text formatting */}
       <SafeAreaView style={styles.container}>
         <Text style={styles.ftitle}>We are starting</Text>
-    </SafeAreaView>
+        <Text style={styles.ftitle1}>
+          Morgan's Online Training/Tutorial For Web Development.{" "}
+          <Text
+            style={styles.ftitle2}
+            onPress={() => Alert.alert("rmpowering link  clicked")}
+          >
+            Empowering
+          </Text>{" "}
+          Your Web Development Journey
+        </Text>
+      </SafeAreaView>
     </SafeAreaProvider>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgray',
-    
+    backgroundColor: "lightgray",
   },
-  ftitle:{
+  ftitle: {
     fontSize: 40,
     fontWeight: 900,
     color: "red",
-  }
-  
-  
+    textAlign: "center",
+  },
+  ftitle1: {
+    fontSize: 20,
+    fontWeight: 300,
+    color: "black",
+    textAlign: "left",
+  },
+  ftitle2: {
+    fontSize: 20,
+    fontWeight: 300,
+    color: "red",
+    textAlign: "justify",
+    textDecorationLine: "underline",
+  },
 });
