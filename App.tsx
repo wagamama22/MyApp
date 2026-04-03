46ecf73147ad594065551995cc8b60274908ac33
wagamama22 <wagajob@gmail.com>
2026-04-03 14:35:06 +0100

    Add full project

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>We are starting now</Text>
     </View>
   );
 }
